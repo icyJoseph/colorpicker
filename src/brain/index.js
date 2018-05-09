ec30b07a921832533dd12757e86bfc25c2afc09d
icyJoseph <sephxd1234@gmail.com>
2018-05-09 07:24:17 +0200

    feat: create instance of Neural Network and helpers

---
+++ src/brain/index.js
@@ -0,0 +1,21 @@
+import { NeuralNetwork } from "brain.js";
+
+export const Net = () => {
+  const brain = new NeuralNetwork();
+  return {
+    brain,
+    asyncTrain: (data, options) => brain.trainAsync(data, options)
+  };
+};
+
+export const normalizer = ({ input, output }) => {
+  return { input: normal(input), output: normal(output) };
+};
+
+export const normal = ({ r, g, b, a }) => {
+  return { r: r / 255, g: g / 255, b: b / 255, a };
+};
+
+export const amplify = ({ r, g, b, a }) => {
+  return { r: 255 * r, g: g * 255, b: b * 255, a };
+};
